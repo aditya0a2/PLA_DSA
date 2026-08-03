c8f79efdadd5aac6855706ed32d5eac9add1d405
Aditya Kumar <sp6700585@gmail.com>
2026-08-03 23:47:31 +0530

    Add Java solution for Codeforces 282A - Bit++

---
+++ 282A.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class BitPlusPlus {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        if (scanner.hasNextInt()) {
+            int n = scanner.nextInt();
+            int x = 0;
+            
+            for (int i = 0; i < n; i++) {
+                String statement = scanner.next();
+                if (statement.contains("+")) {
+                    x++;
+                } else {
+                    x--;
+                }
+            }
+            
+            System.out.println(x);
+        }
+        
+        scanner.close();
+    }
+}
